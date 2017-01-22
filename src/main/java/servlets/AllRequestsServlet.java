7fe4198a903119ca6083601c6930e4afb33903c2
Vladimir Shkerin <vladshkerin@gmail.com>
2017-01-22 20:05:47 +0300

    Home work 01

---
+++ src/main/java/servlets/AllRequestsServlet.java
@@ -0,0 +1,34 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Сервлет простой динамической страницы
+ *
+ * @author Vladimir Shkerin
+ * @since 22.01.2017
+ */
+public class AllRequestsServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+
+        String value = request.getParameter("key");
+        if (value != null) {
+            response.getWriter().print(value);
+        }
+
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+    }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+    }
+
+}
