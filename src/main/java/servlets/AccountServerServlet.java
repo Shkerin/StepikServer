93cf2c85c6c3ab3dc47e5717abb1979c012d447f
Vladimir Shkerin <vladshkerin@gmail.com>
2017-02-09 09:21:16 +0300

    Add servlet AccountServerServlet

---
+++ src/main/java/servlets/AccountServerServlet.java
@@ -0,0 +1,31 @@
+package servlets;
+
+import base.AccountServer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet for account management
+ *
+ * @author Vladimir Shkerin
+ * @since 09.02.2017
+ */
+public class AccountServerServlet extends HttpServlet {
+
+    public static final String PAGE_URL = "/home";
+    private AccountServer accountServer;
+
+    public AccountServerServlet(AccountServer accountServer) {
+        this.accountServer = accountServer;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        resp.getWriter().println(accountServer.getUserLimit());
+    }
+}
